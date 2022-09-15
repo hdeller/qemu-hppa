5cfe5c7e9c07c669da86281f464ad57a865e336a
Helge Deller <deller@gmx.de>
2022-09-15 12:02:38 +0200

    linux-user/hppa: Detect and handle the glibc ABORT_INSTRUCTION (iitlbp)
    The glibc on the hppa platform uses the "iitlbp %r0,(%sr0, %r0)"
    assembler instruction as ABORT_INSTRUCTION.
    If this (in userspace context) illegal assembler statement is found,
    avoid the qemu register dump and report the failure to userspace the
    same way as the Linux kernel on physical hardware.
    
    For other illegal instructions report TARGET_ILL_ILLOPC instead of
    TARGET_ILL_ILLOPN as si_code.
    
    Signed-off-by: Helge Deller <deller@gmx.de>

--- linux-user/hppa/cpu_loop.c
+++ linux-user/hppa/cpu_loop.c
@@ -147,15 +147,20 @@ void cpu_loop(CPUHPPAState *env)
             force_sig_fault(TARGET_SIGSEGV, TARGET_SEGV_MAPERR, env->iaoq_f);
             break;
         case EXCP_ILL:
-            EXCP_DUMP(env, "qemu: got CPU exception 0x%x - aborting\n", trapnr);
-            force_sig_fault(TARGET_SIGILL, TARGET_ILL_ILLOPN, env->iaoq_f);
+            EXCP_DUMP(env, "qemu: CPU exception %#x\n", trapnr);
+            force_sig_fault(TARGET_SIGILL, TARGET_ILL_ILLOPC, env->iaoq_f);
             break;
         case EXCP_PRIV_OPR:
-            EXCP_DUMP(env, "qemu: got CPU exception 0x%x - aborting\n", trapnr);
-            force_sig_fault(TARGET_SIGILL, TARGET_ILL_PRVOPC, env->iaoq_f);
+            /* check for glibc ABORT_INSTRUCTION "iitlbp %r0,(%sr0, %r0)" */
+            if (env->cr[CR_IIR] == 0x04000000) {
+		    force_sig_fault(TARGET_SIGILL, TARGET_ILL_ILLOPC, env->iaoq_f);
+            } else {
+		    EXCP_DUMP(env, "qemu: CPU exception %#x\n", trapnr);
+		    force_sig_fault(TARGET_SIGILL, TARGET_ILL_PRVOPC, env->iaoq_f);
+            }
             break;
         case EXCP_PRIV_REG:
-            EXCP_DUMP(env, "qemu: got CPU exception 0x%x - aborting\n", trapnr);
+            EXCP_DUMP(env, "qemu: CPU exception %#x\n", trapnr);
             force_sig_fault(TARGET_SIGILL, TARGET_ILL_PRVREG, env->iaoq_f);
             break;
         case EXCP_OVERFLOW:
