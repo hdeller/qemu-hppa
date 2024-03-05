6aa06c130c45cb8ed11233fd6391ebb4d4030fb7
Sven Schnelle <svens@stackframe.org>
2024-03-05 19:45:10 +0100

    target/hppa: fix shrp for wide mode
    Signed-off-by: Sven Schnelle <svens@stackframe.org>

--- target/hppa/translate.c
+++ target/hppa/translate.c
@@ -3538,7 +3538,7 @@ static bool trans_shrp_sar(DisasContext *ctx, arg_shrp_sar *a)
     /* Install the new nullification.  */
     cond_free(&ctx->null_cond);
     if (a->c) {
-        ctx->null_cond = do_sed_cond(ctx, a->c, false, dest);
+        ctx->null_cond = do_sed_cond(ctx, a->c, a->d, dest);
     }
     return nullify_end(ctx);
 }
@@ -3581,7 +3581,7 @@ static bool trans_shrp_imm(DisasContext *ctx, arg_shrp_imm *a)
     /* Install the new nullification.  */
     cond_free(&ctx->null_cond);
     if (a->c) {
-        ctx->null_cond = do_sed_cond(ctx, a->c, false, dest);
+        ctx->null_cond = do_sed_cond(ctx, a->c, a->d, dest);
     }
     return nullify_end(ctx);
 }
