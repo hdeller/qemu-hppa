a5e03d1fce0a729a85cfa87d378dc2fe9c21e0f5
Helge Deller <deller@gmx.de>
2024-05-17 22:32:02 +0200

    use constant value for HPPA64_DIAG_SPHASH_ENAB

--- hw/hppa/machine.c
+++ hw/hppa/machine.c
@@ -665,7 +665,7 @@ static void hppa_machine_reset(MachineState *ms, ShutdownCause reason)
         /* reset diagnose registers and enable sr-hashing */
         memset(&cpu[i]->env.dr, 0, sizeof(cpu[i]->env.dr));
         if (hppa_is_pa20(&cpu[0]->env)) {
-            cpu[i]->env.dr[2] = 0x200; /* DIAG_SPHASH_ENAB (bit 54) */
+            cpu[i]->env.dr[2] = HPPA64_DIAG_SPHASH_ENAB;
         }
     }
 
