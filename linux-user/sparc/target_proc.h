ce5b701bc5fca695917e72656733cc6ffc784f50
Helge Deller <deller@gmx.de>
2025-01-08 22:11:58 +0100

    linux-user: Add missing /proc/cpuinfo fields for sparc
    Add some missing fields which may be parsed by userspace
    applications.
    
    Signed-off-by: Helge Deller <deller@gmx.de>

--- linux-user/sparc/target_proc.h
+++ linux-user/sparc/target_proc.h
@@ -8,7 +8,23 @@
 
 static int open_cpuinfo(CPUArchState *cpu_env, int fd)
 {
+    int i, num_cpus;
+
+    num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+
+    dprintf(fd, "cpu\t\t: TI UltraSparc II QEMU (BlackBird)\n");
+    dprintf(fd, "fpu\t\t: UltraSparc II integrated FPU\n");
+    dprintf(fd, "promlib\t\t: Version 3 Revision 17\n");
+    dprintf(fd, "prom\t\t: 3.17.0\n");
     dprintf(fd, "type\t\t: sun4u\n");
+    dprintf(fd, "ncpus probed\t: %d\n", num_cpus);
+    dprintf(fd, "ncpus active\t: %d\n", num_cpus);
+    dprintf(fd, "MMU Type\t: Spitfire\n");
+    dprintf(fd, "State:\n");
+    for (i = 0; i < num_cpus; i++) {
+        dprintf(fd, "CPU%d:\t\t: online\n", i);
+    }
+
     return 0;
 }
 #define HAVE_ARCH_PROC_CPUINFO
